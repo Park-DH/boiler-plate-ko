f026ec80a131722241d9a3523e655bda979321ec
PARK-DH <dxngxx@naver.com>
2023-05-28 18:39:02 +0900

    #10

--- models/User.js
+++ models/User.js
@@ -1,37 +1,54 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true,     // space 없애주는 역할
-        unique: 1       // 똑같은 이메일은 못 쓰게
-    },
-    pasword: {
-        type: String,
-        minlength: 5
-    },
-    lastname: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
+	name: {
+		type: String,
+		maxlength: 50,
+	},
+	email: {
+		type: String,
+		trim: true, // space 없애주는 역할
+		unique: 1, // 똑같은 이메일은 못 쓰게
+	},
+	pasword: {
+		type: String,
+		minlength: 5,
+	},
+	lastname: {
+		type: String,
+		maxlength: 50,
+	},
+	role: {
+		type: Number,
+		default: 0,
+	},
+	image: String,
+	token: {
+		type: String,
+	},
+	tokenExp: {
+		type: Number,
+	},
+});
 
-})
+userSchema.pre("save", function (next) {
+	var user = this;
+	if (user.isModified("password")) {
+		//비밀번호를 암호화 시킨다.
+		bcrypt.genSalt(saltRounds, function (err, salt) {
+			if (err) return next(err);
 
-const User = mongoose.model('User', userSchema)
+			bcrypt.hash(user.password, salt, function (err, hash) {
+				if (err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	}
+});
 
-module.exports = { User }
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
