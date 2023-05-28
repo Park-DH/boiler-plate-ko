ee7971bb0cfa6c9f2c8aea0effc0a6aa04011dd5
PARK-DH <dxngxx@naver.com>
2023-05-28 18:28:03 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://DonghyunP:baxter5736@likelion.pryfnse.mongodb.net/?retryWrites=true&w=majority'
-).then(() => console.log('MongoDB Connected...'))
- .catch(err => console.log(err))
+const config = require("./config/key");
 
+const { User } = require("./models/User");
 
-app.get('/', (req, res) => { res.send('Hello World!') })
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => { console.log(`Example app listening on port ${port}`) })
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+	.connect(config.mongoURI)
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+	res.send("Hello World___");
+});
+
+app.post("/register", async (req, res) => {
+	//회원가입시 필요 정보를 client에서 가져오면
+	//데이터베이스에 삽입한다
+
+	//body parser를 통해 body에 담긴 정보를 가져온다
+	const user = new User(req.body);
+
+	//mongoDB 메서드, user모델에 저장
+	const result = await user
+		.save()
+		.then(() => {
+			res.status(200).json({
+				success: true,
+			});
+		})
+		.catch((err) => {
+			res.json({ success: false, err });
+		});
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
